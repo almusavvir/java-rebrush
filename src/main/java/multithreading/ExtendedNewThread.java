c0845a95266efb80e4ce037196554111a596adf0
almusavvir/devbase <almusavvir.siddiqui@gmail.com>
2025-11-04 21:57:03 +0530

    added more code

---
+++ src/main/java/multithreading/ExtendedNewThread.java
@@ -0,0 +1,5 @@
+package multithreading;
+
+public class ExtendedNewThread extends NewThread implements Runnable {
+
+}
--- src/main/java/multithreading/NewThread.java
+++ src/main/java/multithreading/NewThread.java
@@ -1,5 +1,6 @@
 package multithreading;
 
+import java.sql.SQLOutput;
 import java.time.LocalDateTime;
 import java.time.chrono.ChronoLocalDate;
 import java.time.format.DateTimeFormatter;
@@ -9,22 +10,23 @@ public class NewThread extends Thread {
     @Override
     public void run() {
         System.out.println("RUNNING");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
         NewThread t1 = new NewThread();
 
-        System.out.println("Below listed are live Thread states ⬇️\n");
-
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss:SSSSSSS");
-
-        System.out.println(now.format(formatter));
+        System.out.println("\nBelow listed are live Thread states ⬇️\n");
         System.out.println(t1.getState());
-        System.out.println(now.format(formatter));
         t1.start();
         System.out.println(t1.getState());
-        System.out.println(now.format(formatter));
+        Thread.sleep(100);
+        System.out.println(t1.getState());
+        t1.join();
+        System.out.println(t1.getState());
     }
-
 }
