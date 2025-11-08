83204419f7065f9d130efea4823a41a85d67a461
almusavvir <almusavvir.siddiqui@gmail.com>
2025-11-08 13:10:56 +0530

    add more modules

---
+++ src/main/java/arrays/OneD.java
@@ -0,0 +1,10 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class OneD {
+    public static void main(String[] args) {
+        int[] numarr = {65,34,69,75};
+        System.out.println(Arrays.toString(numarr));
+    }
+}
