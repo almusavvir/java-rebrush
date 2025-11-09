6c58483650f4ccbea73b6c41fcb874777814100d
almusavvir <almusavvir.siddiqui@gmail.com>
2025-11-09 08:54:27 +0530

    modify OneD array code

--- src/main/java/arrays/OneD.java
+++ src/main/java/arrays/OneD.java
@@ -6,5 +6,12 @@ public class OneD {
     public static void main(String[] args) {
         int[] numarr = {65,34,69,75};
         System.out.println(Arrays.toString(numarr));
+
+        int[] numarr2 = new int[25];
+
+        numarr2[0] = 100;
+
+        System.out.println(Arrays.toString(numarr2));
+
     }
 }
