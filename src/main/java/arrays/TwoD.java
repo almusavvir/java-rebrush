f3288d3536ae04b11cf48123fb9f33bd5f3990e0
almusavvir <almusavvir.siddiqui@gmail.com>
2025-11-09 12:09:57 +0530

    modify twoD module

--- src/main/java/arrays/TwoD.java
+++ src/main/java/arrays/TwoD.java
@@ -1,13 +1,32 @@
 package arrays;
 
-import java.util.Arrays;
+import linespace.lp;
 
 public class TwoD {
     public static void main(String[] args) {
-        int[][] TwoD = new int[10][10];
+        /*int[][] TwoD = new int[10][10];
         TwoD[0][1] = 100;
         TwoD[1][0] = 200;
 
-        System.out.println(Arrays.toString(TwoD));
+        System.out.println(Arrays.toString(TwoD));*/
+
+        String[] veggies = {"onions", "carrots" , "beetroot"};
+        String[] fruits = {"apple", "banana", "mangoes", "pomegranate", "grape"};
+        String[] meat = {"chicken", "beef", "lamb", "fish"};
+
+        String[][] groceries = {veggies, fruits, meat};
+
+        /*for(String item: veggies) {
+            System.out.println(item);
+        }*/
+
+        for (String[] category: groceries) {
+            lp.one();
+            for(String item: category) {
+                System.out.print(item + " ");
+            }
+        }
+        lp.one();
+
     }
 }
---
+++ src/main/java/linespace/lp.java
@@ -0,0 +1,7 @@
+package linespace;
+
+public class lp {
+    public static void one() {
+        System.out.println();
+    }
+}
